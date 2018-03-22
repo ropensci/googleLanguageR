b940dade1f5d2d9f72806bf6b7d76cc2df608e76
Mark Edmondson <github@markedmondson.me>
2018-03-22 17:17:06 +0100

    get rid of a NOTE

--- R/version.R
+++ R/version.R
@@ -1,6 +1,6 @@
 # used to specify the API endpoint - on GitHub beta, on CRAN, stable
 get_version <- function(){
-  if(grepl("\\.9...$", packageVersion("googleLanguageR"))){
+  if(grepl("\\.9...$", utils::packageVersion("googleLanguageR"))){
     version <- "v1beta2"
   } else {
     version <- "v1"
