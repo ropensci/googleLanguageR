2a86fb05e7905d70947e8438d3033350e114bce8
Mark Edmondson <github@markedmondson.me>
2018-08-15 17:11:56 +0200

    Change API version for GitHub tests

--- R/version.R
+++ R/version.R
@@ -1,7 +1,7 @@
 # used to specify the API endpoint - on GitHub beta, on CRAN, stable
 get_version <- function(){
   if(grepl("\\.9...$", utils::packageVersion("googleLanguageR"))){
-    version <- "v1beta2"
+    version <- "v1p1beta1"
   } else {
     version <- "v1"
   }
