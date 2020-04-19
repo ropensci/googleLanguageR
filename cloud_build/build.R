f2172dfcd1abd7369cc224e05184dee39547b13b
Mark Edmondson <github@markedmondson.me>
2020-04-19 20:21:22 +0200

    repo dir

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -14,3 +14,15 @@ cr_deploy_pkgdown(
   cloudbuild_file = "cloud_build/cloudbuild-pkgdown.yml",
   env = "GL_AUTH=/workspace/auth.json"
 )
+
+# add step after cloning:
+# - name: gcr.io/cloud-builders/git
+# id: set git push
+# args:
+#   - remote
+# - set-url
+# - --push
+# - --origin git@github.com:MarkEdmondson1234/googleLanguageR
+# volumes:
+#   - name: ssh
+# path: /root/.ssh
