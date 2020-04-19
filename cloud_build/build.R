2ea86880d2ec097b037da19b05313a20e95567e6
Mark Edmondson <github@markedmondson.me>
2020-04-19 18:15:08 +0200

    test build website

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -2,6 +2,14 @@ library(googleCloudRunner)
 
 cr_deploy_packagetests(
   steps = cr_buildstep_secret("googlelanguager-auth", "/workspace/auth.json"),
+  cloudbuild_file = "cloud_build/cloudbuild-tests.yml",
   timeout = 2400,
   env = c("NOT_CRAN=true","GL_AUTH=/workspace/auth.json")
+)
+
+cr_deploy_pkgdown(
+  steps = cr_buildstep_secret("googlelanguager-auth", "/workspace/auth.json"),
+  secret = "github-ssh",
+  cloudbuild_file = "cloud_build/cloudbuild-pkgdown.yml",
+  env = "GL_AUTH=/workspace/auth.json"
 )
