72c7c84380e559db3c5fafeea9ed3a0e050099dd
Aleksander Dietrichson <dietrichson@gmail.com>
2023-06-01 18:42:55 +0000

    Added support for translating documents

--- R/translate-document.R
+++ R/translate-document.R
@@ -5,12 +5,14 @@
 
 #'
 #' @param d_path path of the document to be translated
-#' @param out_path where to save the translated document
+#' @param output_path where to save the translated document
 #' @param format currently only pdf-files are supported
 #'
 #' @return invisible() on success
 #' @family translations
 #' @import assertthat
+#' @importFrom base64enc base64encode
+#' @importFrom base64enc base64decode
 #' @importFrom utils URLencode
 #' @importFrom googleAuthR gar_api_generator
 #' @importFrom tibble as_tibble
@@ -18,7 +20,7 @@
 #' @export
 gl_translate_document <- function(d_path,
                                   target = "es-ES",
-                                  out_path = "out.pdf",
+                                  output_path = "out.pdf",
                                   format = c("pdf"),
                                   source = 'en-UK',
                                   model = c("nmt", "base"),
@@ -27,7 +29,7 @@ gl_translate_document <- function(d_path,
 
   ## Checks
   assert_that(is.character(d_path),
-              is.character(out_path),
+              is.character(output_path),
               is.string(target),
               is.string(source))
 
@@ -61,45 +63,12 @@ gl_translate_document <- function(d_path,
                                 "POST"
                                 )
 
-
-
   me <- tryCatch(call_api(the_body = payload))
-  # me <- tryCatch(call_api(the_body = pars),
-  #                error = function(ex){
-  #                  if(grepl(catch_errors,
-  #                           ex$message)){
-  #                    my_message(ex$message, level = 3)
-  #                    my_message("Attempting to split into several API calls", level = 3)
-  #                    # Reduce(rbind, lapply(t_string, gl_translate,
-  #                    #                      format = format,
-  #                    #                      target = target,
-  #                    #                      source = source,
-  #                    #                      model = model))
-  #                  } else if(grepl("User Rate Limit Exceeded",
-  #                                  ex$message)){
-  #                    my_message("Characters per 100 seconds rate limit reached, waiting 10 seconds...",
-  #                               level = 3)
-  #                    Sys.sleep(10)
-  #                    ## try again
-  #                    gl_translate(t_string,
-  #                                 format = format,
-  #                                 target = target,
-  #                                 source = source,
-  #                                 model = model)
-  #                  } else {
-  #                    stop(ex$message, call. = FALSE)
-  #                  }
-  #                })
-  #
-  me
 
-}
 
-if(interactive()){
-  library(googleAuthR)
-  gl_auth("_api-keys/google_api_key.json") -> my_test
-  library(dplyr)
-  library(assertthat)
-  gl_translate_document("./notes/test-doc.pdf") %>% print
+  me$content$documentTranslation[[1]] %>%
+   base64decode() %>%
+    writeBin(output_path)
+
 }
 
